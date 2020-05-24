d8e0dc2aa9044ea4f1364abcdb02f488da630c35
YagoErnandes <yago700@gmail.com>
2020-05-24 12:18:24 -0300

    first

---
+++ config/metadata.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: "Yago Ernandes",
+  siteUrl: `https://www.yagoernandes.com`,
+  description: "Portfólio online de Yago Ernandes",
+  social: [
+    {
+      name: "github",
+      url: "https://github.com/yagoernandes",
+    },
+    {
+      name: "twitter",
+      url: "https://twitter.com/yagoernandes",
+    },
+  ],
+}
--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,82 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+const siteMetadata = require("./config/metadata")
+
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata,
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    ////////////////////////////////////////////////////
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // Setting a color is optional.
+        color: `tomato`,
+        // Disable the loading spinner.
+        showSpinner: false,
+      },
+    },
+    ////////////////////////////////////////////////////
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 1000,
+        // Defers execution of google analytics script after page load
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "example.com",
+      },
+    },
+    ////////////////////////////////////////////////////
+    // {
+    //   resolve: `gatsby-plugin-prefetch-google-fonts`,
+    //   options: {
+    //     fonts: [
+    //       {
+    //         family: `Roboto`,
+    //         subsets: [`300`, `500`, `700`],
+    //       },
+    //     ],
+    //   },
+    // },
+    ////////////////////////////////////////////////////
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `YagoErnandes`,
+        short_name: `YagoErnandes`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#7159c1`,
+        display: `standalone`,
+        icon: `src/assets/icon.png`, //512x512
+      },
+    },
+    ////////////////////////////////////////////////////
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        displayName: process.env.NODE_ENV !== "production",
+      },
+    },
+    ////////////////////////////////////////////////////
+    {
+      resolve: `gatsby-plugin-page-creator`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        ignore: `**/styles.js`,
+      },
+    },
+    ////////////////////////////////////////////////////
+    `gatsby-plugin-offline`,
+  ],
 }
